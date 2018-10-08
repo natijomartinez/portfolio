bb04b326eb6dd051bb87be40e5e24b431761c19b
Natalie Martinez <nmartinez@mediamatters.org>
2018-10-08 18:28:00 -0400

    collumns FIXED

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+$(".before").click(function() {
+	$(".before").fadeOut(1000);
+
+})
+
+	// when you click on the after image, fade the before image in
+
+
+$(".after").click(function() {
+	$(".before").fadeIn(1000);
+
+})
+
+    
+});
